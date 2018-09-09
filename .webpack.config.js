48557d68d87f8c7ad1d1371ffdd0d7683dee0061
ohnaka <ohnaka@no4.co.jp>
2018-09-10 02:00:15 +0900

    add webpack sourcemap

--- .webpack.config.js
+++ .webpack.config.js
@@ -1,5 +1,8 @@
+const isProd   = (process.env.NODE_ENV === 'production');
+
 module.exports = {
   mode  : process.env.NODE_ENV,
+  devtool: !isProd ? 'source-map' : false,
   module: {
     rules: [
       {
